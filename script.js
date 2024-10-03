86877774c1d669966fa34049da60fc39cb5213c0
Guilherme <guilherme.a.caseiro@gmail.com>
2024-10-03 11:34:01 -0300

    Criando objeto array e metodo

--- script.js
+++ script.js
@@ -22,9 +22,32 @@ while (w < 20)
 //função
 
 function Ola(nome) {
+    // string literal ou template string ${} chama a variável
     console.log(`Seja Bem Vindo a ${nome}`)
     alert(`seja bem vindo a ${nome}`)
     document.write(`seja bem vindo a ${nome}`)
 }
 
+//Executando a função Ola passando o valor
 Ola("Fiap")
+
+//Arrow Function 
+
+const hello = () => (console.log("Função arrow function"))
+
+hello()
+
+//Array = [], Objeto= {} e métodos =()
+
+let jogadores = [
+    { nome: "Huginho", idade: 17 },
+    { nome: "Zezinho", idade: 18 },
+    { nome: "Luizinho", idade: 19 }
+]
+
+let listarJogadores = jogadores.map(function (item) {
+    //return item.nome;
+    return item.idade;
+
+})
+console.log(listarJogadores)
