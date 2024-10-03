14fc2d7bb643cc259b36ea17ccc7ba584d9b4212
Guilherme <guilherme.a.caseiro@gmail.com>
2024-10-03 11:24:14 -0300

    Criando função em Javascript

---
+++ script.js
@@ -0,0 +1,30 @@
+//Estrutura de Rpetição For
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
+//While
+
+let f = 0;
+while (f > 10) {
+    console.log(w)
+    f++;
+}
+
+//do while
+let w = 0;
+
+do {
+    console.log(w);
+    w++;
+}
+while (w < 20)
+
+//função
+
+function Ola(nome) {
+    console.log(`Seja Bem Vindo a ${nome}`)
+    alert(`seja bem vindo a ${nome}`)
+    document.write(`seja bem vindo a ${nome}`)
+}
+
+Ola("Fiap")
